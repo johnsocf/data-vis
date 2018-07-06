eed7a042aab056e1cbfcdf3471c6d852f4295642
johnsocf <cfjohnson@earthlink.net>
2018-07-06 13:14:36 -0600

    Auto-generated commit

---
+++ assets/colorSets/color-legend.ts
@@ -0,0 +1,30 @@
+export const electricityChildren = [
+  'electricityProduction',
+  'Electricity production from coal sources (% of total)',
+  'Electricity production from hydroelectric sources (% of total)',
+  'Electricity production from natural gas sources (% of total)',
+  'Electricity production from nuclear sources (% of total)',
+  'Electricity production from oil sources (% of total)',
+  'Electricity production from renewable sources, excluding hydroelectric (% of total)',
+  'Renewable electricity output (% of total electricity output)'
+];
+
+export const emissionsChildren = [
+  'emissions',
+  'CO2 emissions from gaseous fuel consumption (% of total)',
+  'CO2 emissions from solid fuel consumption (% of total)',
+  'Methane emissions (% change from 1990)',
+  'Nitrous oxide emissions (% change from 1990)',
+  'Other greenhouse gas emissions (% change from 1990)',
+  'Total greenhouse gas emissions (% change from 1990)'
+];
+
+export const populationChildren = [
+  'population',
+  'Population in urban agglomerations of more than 1 million (% of total population)',
+  'Urban population (% of total)',
+  'Urban population growth (annual %)'
+];
+
+
+
---
+++ assets/colorSets/color-set.ts
@@ -0,0 +1,79 @@
+export const colorSet20b = [
+'#1f77b4',
+'#aec7e8',
+'#ff7f0e',
+'#ffbb78',
+'#2ca02c',
+'#98df8a',
+'#d62728',
+'#ff9896',
+'#9467bd',
+'#c5b0d5',
+'#8c564b',
+'#c49c94',
+'#e377c2',
+'#f7b6d2',
+'#7f7f7f',
+'#c7c7c7',
+'#bcbd22',
+'#dbdb8d',
+'#17becf',
+'#9edae5'
+];
+
+export const colorSetRed = [
+'#f7f4f9',
+'#e7e1ef',
+'#d4b9da',
+'#c994c7',
+'#df65b0',
+'#e7298a',
+'#ce1256',
+'#67001f',
+'#67001f'
+];
+
+export const redSetComplimentWhiteText = [
+  '#67001f',
+  '#67001f',
+  '#ce1256',
+  '#e7298a'
+];
+
+export const colorSetGreen = [
+'#ffffe5',
+'#f7fcb9',
+'#d9f0a3',
+'#addd8e',
+'#78c679',
+'#41ab5d',
+'#238443',
+'#006837',
+'#004529'
+];
+
+export const greenSetComplimentWhiteText = [
+  '#004529',
+  '#006837',
+  '#238443',
+  '#41ab5d'
+];
+
+export const colorSetBlue = [
+'#fff7fb',
+'#ece7f2',
+'#d0d1e6',
+'#a6bddb',
+'#74a9cf',
+'#3690c0',
+'#0570b0',
+'#045a8d',
+'#023858'
+];
+
+export const blueSetComplimentWhiteText = [
+  '#023858',
+  '#045a8d',
+  '#0570b0',
+  '#3690c0'
+];
